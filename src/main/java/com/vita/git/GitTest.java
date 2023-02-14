de08a10f6686e2e1b8513680c7e16631615e4e62
Vita <campus61@163.com>
2023-02-14 11:13:41 +0800

    first commit

---
+++ src/main/java/com/vita/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.vita.git;
+
+/**
+ * Created by zhaoxi on 2023/2/14.
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
